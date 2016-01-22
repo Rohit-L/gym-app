5b3377ab793ee83316ada3502f07be9f90b44f98
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-01-21 22:34:16 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+/*************************************************************************
+ * 
+ *
+ *************************************************************************
+ *
+ * @description Server logic
+ *
+ *
+ * @author Rohit Lalchandani
+ *
+ *
+ *************************************************************************/
+
+/************************
+ * Creates a web server *
+ ************************/
+var express = require('express'), // Creates an express instance
+    app = express(),
+    server = require('http').createServer(app), // Creates a web server
+    io = require('socket.io').listen(server); // Creates a socket.io instance
+
+server.listen(process.env.PORT || 3000); // Listening port for server
+console.log("Listening");
+
+/* Serve static content */
+app.use(express.static('public'));
+
+/* Set Jade view engine */
+app.set('view engine', 'jade');
+
+// Routing for webpages
+app.get('/', function(req, res) {
+  //res.sendfile("chessboard.html");
+  res.render('index');
+});
