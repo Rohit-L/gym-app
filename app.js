71cf61ca4e588725b4e8295560e2bc35820b0018
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-01-23 12:14:20 -0800

    Setting of login

--- app.js
+++ app.js
@@ -4,31 +4,20 @@
  * @description Server logic
  * @author Rohit Lalchandani
  *************************************************************************/
-
-/************************
- * Creates a web server *
- ************************/
 var express = require('express'), // Creates an express instance
     app = express(),
     server = require('http').createServer(app), // Creates a web server
-    io = require('socket.io').listen(server); // Creates a socket.io instance
     pg = require('pg'); // PostgreSQL
     passport = require('passport');
     config = require('./fb.js');
     FacebookStrategy = require('passport-facebook').Strategy;
     cookieParser = require('cookie-parser')
     session = require('express-session')
+    bodyParser = require('body-parser')
 
-
-// serialize and deserialize
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-// config
+/********************
+ ** Authentication **
+ ********************/
 passport.use(new FacebookStrategy({
   clientID: config.appID,
   clientSecret: config.appSecret,
@@ -41,9 +30,23 @@ passport.use(new FacebookStrategy({
   }
 ));
 
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-
+/************************
+ * Creates a web server *
+ ************************/
 server.listen(process.env.PORT || 3000); // Listening port for server
 console.log("Listening at localhost");
 
@@ -53,31 +56,52 @@ app.use(express.static('public'));
 /* Set Jade view engine */
 app.set('view engine', 'jade');
 
-// Configure App
-app.use(cookieParser());
-app.use(session({ secret: 'berkeleyEECS' }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-// Routing for webpages
-app.get('/', function(req, res) {
-  res.render('index');
+//Router code
+app.get('/', function(req, res){
+  res.render('login');
 });
-
-app.get('/dashboard', function(req, res) {
-  res.render('dashboard');
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
 });
-
-// Database Setup
-app.get('/db', function (request, response) {
-  console.log(process.env.DATABASE_URL)
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { console.log(result); }
-    });
+//Passport Router
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+       successRedirect : '/dashboard',
+       failureRedirect: '/login'
+  }),
+  function(req, res) {
+    res.redirect('/');
   });
-})
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+// // Routing for webpages
+// app.get('/', function(req, res) {
+//   res.render('index');
+// });
+//
+// app.get('/dashboard', function(req, res) {
+//   res.render('dashboard');
+// });
+//
+// // Database Setup
+// app.get('/db', function (request, response) {
+//   console.log(process.env.DATABASE_URL)
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { console.log(result); }
+//     });
+//   });
+// })
--- fb.js
+++ fb.js
@@ -2,5 +2,5 @@
 module.exports = {
   'appID' : '550698448428271',
   'appSecret' : '0e385c3200343c3cbfaf7e59675ab111',
-  'callbackUrl' : 'https://shielded-shore-54060.herokuapp.com/'
+  'callbackURL' : 'https://shielded-shore-54060.herokuapp.com/'
 }
