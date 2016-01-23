fa18515edbd2d5262e8cb795fa57955308801d21
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-01-22 20:46:41 -0800

    Setting up PostgreSQL

--- app.js
+++ app.js
@@ -37,3 +37,18 @@ app.get('/', function(req, res) {
 app.get('/dashboard', function(req, res) {
   res.render('dashboard');
 });
+
+// Database Setup
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
